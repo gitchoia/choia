4b1f981ac812f1ccf1cfd995e82ef73061cd9139
gitchoia <63134235+gitchoia@users.noreply.github.com>
2020-04-04 18:40:52 -0400

    Add files via upload

---
+++ build/precache-manifest.9213bb52ac5a4f70d6dbc059d317005a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d43dc62a63d2e44f6bab6d0a848fcb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ee8cc4a55f77b702316",
+    "url": "/static/css/main.7629a1a8.chunk.css"
+  },
+  {
+    "revision": "602ae8c8cf9fb525be20",
+    "url": "/static/js/2.31365275.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31365275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee8cc4a55f77b702316",
+    "url": "/static/js/main.eae611de.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b3dd4dacf4a7a446259356103ab997d",
+    "url": "/static/media/me.0b3dd4da.svg"
+  }
+]);
